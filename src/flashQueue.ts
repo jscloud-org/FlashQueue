07ae6a80dc5e5441f62f92354751542a131dc50b
Jerry S Joseph <jerin.sebastian153@gmail.com>
2021-11-13 12:44:26 +0530

    changed local vars to private in FlashQueue class

--- src/flashQueue.ts
+++ src/flashQueue.ts
@@ -1,9 +1,9 @@
 
 export default class FlashQueue<T>{
 
-    storage:object
-    head:number
-    tail:number
+    private storage: object
+    private head: number
+    private tail: number
 
     constructor(){
         this.storage={}
@@ -11,41 +11,53 @@ export default class FlashQueue<T>{
     }
 
     /**
-     * Enqueue or insert an item
+     * Enqueue or insert an item to the queue
      * @param item 
-     * @returns 
+     * @returns true if operation successfull
      */
     public enqueue(item:T):boolean{
         const index=this.tail;
-        this.storage[index]=item;
+        this.setItem(index, item);
         this.tail++;
         return true;
     }
 
     /**
-     * Dequeue or remove an item
-     * @returns 
+     * Dequeue next item in Order
+     * @returns {T} next item in queue
      */
     public dequeue():T|undefined{
         if(this.head===this.tail)
             return undefined;
-        const item= this.storage[this.head];
-        delete this.storage[this.head];
+        const item = this.getItem(this.head);
+        this.deleteItem(this.head);
         this.head++;
         if(this.head===this.tail)
             this.head=this.tail=0;
         return item;
     }
 
-    public getObject(){
-        return this.storage;
+    private getItem(idx: number) {
+        //@ts-ignore
+        return this.storage[idx];
     }
+
+    private deleteItem(idx: number) {
+        //@ts-ignore
+        delete this.storage[idx];
+    }
+
+    private setItem(idx: number, item: any) {
+        //@ts-ignore
+        this.storage[idx] = item;
+    }
+
     /**
-     * Peek the next item
-     * @returns 
+     * Peek the next item but not remove it from the queue
+     * @returns next item in queue
      */
     public peek(){
-        return this.storage[this.head];
+        return this.getItem(this.head);
     }
 
     public size(){
